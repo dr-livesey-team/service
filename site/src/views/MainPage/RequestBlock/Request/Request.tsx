45eb174faabdd6b917eb15c11ae89f83889235bf
1ldaun <sharfull33@gmail.com>
2022-11-06 23:59:48 +0300

    feat: updated request page

--- site/src/views/MainPage/RequestBlock/Request/Request.tsx
+++ site/src/views/MainPage/RequestBlock/Request/Request.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import S from "./request.module.scss";
 import classnames from "classnames";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Request: React.FC<{ minRequest?: boolean; anomaly: any; isActive?: boolean }> = ({ minRequest, anomaly, isActive }) => {
+const Request: React.FC<{
+  minRequest?: boolean;
+  anomaly: any;
+  isActive?: boolean;
+}> = ({ minRequest, anomaly, isActive }) => {
+  const date = anomaly?.opening_date;
+  date?.slice(0, 10);
   return (
-    <Link to={'/request?id=' + anomaly?.id} id={'anom' + anomaly?.id} className={classnames(S.request, minRequest ? S.minimal : "", isActive ? S.active : "")}>
+    <Link
+      to={"/request?id=" + anomaly?.id}
+      id={"anom" + anomaly?.id}
+      className={classnames(
+        S.request,
+        minRequest ? S.minimal : "",
+        isActive ? S.active : ""
+      )}
+    >
       <td>{anomaly?.number}</td>
       <td>{anomaly?.fault_name}</td>
-      <td>{anomaly?.opening_date}</td>
+      <td>{date}</td>
       {!minRequest && <td>{anomaly?.management_company_name}</td>}
       {!minRequest && <td>{anomaly?.service_organization_name}</td>}
     </Link>
